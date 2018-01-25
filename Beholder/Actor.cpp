045c1a8c71d03b7bfce31afa707c8496fe09e5a5
Nakroma <tarackobarack@gmail.com>
2018-01-25 17:49:05 +0100

    added template and mask loading

--- Beholder/Actor.cpp
+++ Beholder/Actor.cpp
@@ -5,4 +5,12 @@
 Actor::Actor(std::string sActorName, std::string sTemplatePath, std::string sMaskPath = "") {
 
     sName = sActorName;
+
+    mTemplate = cv::imread(sTemplatePath);
+    if (mTemplate.empty()) throw std::invalid_argument("couldnt load template image");
+
+    if (!sMaskPath.empty()) {
+        mMask = cv::imread(sMaskPath);
+        if (mMask.empty()) throw std::invalid_argument("couldnt load mask image");
+    }
 }

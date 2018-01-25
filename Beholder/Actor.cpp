792eaedb5e527949fde59de9052a11c4117e46c8
Nakroma <tarackobarack@gmail.com>
2018-01-25 17:38:41 +0100

    added another default constructor

--- Beholder/Actor.cpp
+++ Beholder/Actor.cpp
@@ -2,6 +2,11 @@
 
 
 // Default constructor
+Actor::Actor(std::string sActorName, cv::Mat mTemplateObj, cv::Mat mMaskObj = cv::Mat()) {
+
+}
+
+// Constructor with paths
 Actor::Actor(std::string sActorName, std::string sTemplatePath, std::string sMaskPath = "") {
 
     sName = sActorName;
--- Beholder/Actor.h
+++ Beholder/Actor.h
@@ -6,6 +6,7 @@
 // Class for actor representation
 class Actor {
     public:
+        Actor(std::string sActorName, cv::Mat mTemplateObj, cv::Mat mMaskObj = cv::Mat());
         Actor(std::string sActorName, std::string sTemplatePath, std::string sMaskPath = "");
 
         std::string sName;

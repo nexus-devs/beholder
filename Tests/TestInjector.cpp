695eef471f565e239832312dc84bd4ea4317dd15
Nakroma <tarackobarack@gmail.com>
2018-01-31 17:34:13 +0100

    added basic injector test

---
+++ Tests/TestInjector.cpp
@@ -0,0 +1,21 @@
+#include "catch.hpp"
+#include "../Beholder/Injector.h"
+
+
+LPCWSTR sTestExe = L"test_window.exe";
+
+// Test file for the injector and general nexus screen
+TEST_CASE("window gets correctly injected", "[injector]") {
+
+    // Open up test .exe
+    STARTUPINFO info = { sizeof(info) };
+    PROCESS_INFORMATION processInfo;
+    BOOL createdProcess = CreateProcess(sTestExe, NULL, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
+
+    REQUIRE(createdProcess);
+
+    // Cleanup
+    TerminateProcess(processInfo.hProcess, 0);
+    CloseHandle(processInfo.hProcess);
+    CloseHandle(processInfo.hThread);
+}

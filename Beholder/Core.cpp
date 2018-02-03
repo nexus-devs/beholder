bb4f46a595683e6cc80e1de507c9a11b3206e4b9
Nakroma <tarackobarack@gmail.com>
2018-02-03 18:45:03 +0100

    added basic injection to core

--- Beholder/Core.cpp
+++ Beholder/Core.cpp
@@ -1,8 +1,11 @@
 #include "Core.h"
 
 
+std::string sNexusScreenPath = "../NexusScreen.dll";
+
+
 // Default constructor
 Core::Core() {
 
-
+    Injector::Inject(20, sNexusScreenPath);
 }
--- Beholder/Core.h
+++ Beholder/Core.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "stdafx.h"
+#include "Injector.h"
 
 
 // Beholder core class
 class Core {
     public:
         Core();
+
+    private:
+        static std::string sNexusScreenPath;
 };

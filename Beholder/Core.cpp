e743c4542cd00834b9ccbad7a93483bc6d22206f
Nakroma <tarackobarack@gmail.com>
2018-02-04 22:53:51 +0100

    fixed retrieval of process id

--- Beholder/Core.cpp
+++ Beholder/Core.cpp
@@ -10,10 +10,11 @@ Core::Core(std::string sWindowName, std::string sWindowClass) {
     LPCSTR sWindowClassConverted = NULL;
     if (!sWindowClass.empty()) sWindowClassConverted = sWindowClass.c_str();
 
-    HANDLE hWindow = FindWindowEx(0, 0, sWindowClassConverted, sWindowName.c_str());
+    HWND hWindow = FindWindowEx(0, 0, sWindowClassConverted, sWindowName.c_str());
     if (!hWindow) throw std::invalid_argument("couldnt find the window [win error: " + std::to_string(GetLastError()) + "]");
 
-    DWORD iProcessId = GetProcessId(hWindow);
+    DWORD iProcessId;
+    if (!GetWindowThreadProcessId(hWindow, &iProcessId)) throw std::runtime_error("couldnt retrieve process id");
 
-    //Injector::Inject(iProcessId, sNexusScreenPath);
+    Injector::Inject(iProcessId, sNexusScreenPath);
 }
